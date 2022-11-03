699487f651f2c097873be6014367251a2ce51669
David Hwang <nomad.hwang@gmail.com>
2022-11-03 16:53:18 +0900

    docs: README.md added

---
+++ README.py
@@ -0,0 +1,6 @@
+# SSH Bastion Management
+
+It's a simple SSH Bastion Management system. It's a work in progress.
+
+- Provisioning with temporary access token retreived from web page
+- Client side no extra dependency, just shell script
